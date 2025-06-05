8277443273d1a9ac796d45056c6e2b8a4ef7036b
Polina Lozova <1polinalozovaya1@gmail.com>
2025-06-05 12:17:39 +0300

    add galeryPage

---
+++ app/(home)/galery/page.js
@@ -0,0 +1,12 @@
+import GaleryPage from '@/pages/GaleryPage';
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <GaleryPage />
+    </div>
+  );
+}
+
+export default page;
