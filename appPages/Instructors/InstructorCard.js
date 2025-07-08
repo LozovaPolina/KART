290108a2eeab5bd094d3c2105710e7ba71594fc0
Evgeniy <myiphone128@mail.ru>
2025-07-08 21:17:38 +0300

    connected apis instructors

--- appPages/Instructors/InstructorCard.js
+++ appPages/Instructors/InstructorCard.js
@@ -1,49 +1,51 @@
-
-import CircleLink from '../../shared/ui/button/CircleLink';
-import Image from 'next/image';
+import CircleLink from "../../shared/ui/button/CircleLink";
+import Image from "next/image";
 import { Link } from "../../i18n/navigation";
-import React from 'react';
-import womenImg from '../../public/assets/image/womenImg.png';
-import instagram from '../../public/assets/svg/insta.svg';
-import gmail from '../../public/assets/svg/gmail-icon.svg';
-import website from '../../public/assets/svg/website-icon.svg';
+import React from "react";
+import womenImg from "../../public/assets/image/womenImg.png";
+import instagram from "../../public/assets/svg/insta.svg";
+import gmail from "../../public/assets/svg/gmail-icon.svg";
+import website from "../../public/assets/svg/website-icon.svg";
 const socialIcons = {
-  mail: <Image src={gmail} alt='gmail' className="w-5 h-5" />,
-  instagram: <Image src={instagram} alt='instagram' className="w-5 h-5" />,
-  globe: <Image src={website} alt='website' className="w-5 h-5" />,
+  mail: <Image src={gmail} alt="gmail" className="w-5 h-5" />,
+  instagram: <Image src={instagram} alt="instagram" className="w-5 h-5" />,
+  other: <Image src={website} alt="website" className="w-5 h-5" />,
 };
 
-export default function InstructorCard({ instructor }) {
+export default function InstructorCard({
+  firstName,
+  lastName,
+  position,
+  country,
+  contacts,
+}) {
   return (
     <div className="flex flex-col p-[15px] bg-[#F5F5F5] shadow rounded-xl hover:shadow-md transition-all">
       <div className="flex gap-4 items-start">
         <Image
           src={womenImg}
-          alt={instructor?.name}
+          alt="instructor image"
           className="object-cover rounded-xl w-[5.625rem] h-[5.625rem] sm:w-[6.25rem] sm:h-[6.25rem]"
         />
 
         <div className="flex-1 flex flex-col gap-2 justify-between text-[#000000] min-h-[100px]">
           <div>
-            <p className="font-semibold">{instructor?.name}</p>
-            {instructor?.role && (
-              <p className="text-sm text-gray-500">{instructor?.role}</p>
-            )}
-            <p className="text-xs text-gray-600">{instructor?.location}</p>
+            <p className="font-semibold">{firstName + " " + lastName}</p>
+            <p className="text-sm text-gray-500">{position}</p>
+            <p className="text-xs text-gray-600">{country}</p>
           </div>
 
-          {/* Social icons at the bottom of text section */}
-          <div className="flex  gap-2 justify-between items-center">
+          <div className="flex gap-2 justify-between items-center">
             <div className="flex gap-2">
-              {instructor?.socials.map((item, i) => (
+              {contacts.map((item, i) => (
                 <Link
                   key={i}
-                  href={item.url}
+                  href={item.value}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="w-[2rem] h-[2rem] bg-gray-100 rounded-xl shadow flex justify-center items-center hover:bg-gray-200 transition"
                 >
-                  {socialIcons[item.type]}
+                  {socialIcons[item.contact_type]}
                 </Link>
               ))}
             </div>
@@ -54,4 +56,3 @@ export default function InstructorCard({ instructor }) {
     </div>
   );
 }
-
--- appPages/Instructors/Instructors.js
+++ appPages/Instructors/Instructors.js
@@ -1,42 +1,86 @@
-"use client"
+"use client";
 
-import HintNavigation from '../../shared/hint-navigation/HintNavigation';
-import Title from '../../shared/ui/title/Title';
+import HintNavigation from "../../shared/hint-navigation/HintNavigation";
+import Title from "../../shared/ui/title/Title";
 
-import InstructorCard from './InstructorCard';
-import Pagination from '../../shared/pagination/Pagination';
+import InstructorCard from "./InstructorCard";
+import Pagination from "../../shared/pagination/Pagination";
 import { useSearchParams } from "next/navigation";
-import { instructors } from '../../data/instructors'
-import Button from '../../shared/ui/button/Button';
-import { Link } from '../../i18n/navigation';
+import Button from "../../shared/ui/button/Button";
+import { Link } from "../../i18n/navigation";
+import { useState, useEffect } from "react";
+import { API_URL } from "../../data/url";
 
-
+import { useLocale } from "next-intl";
 
 export default function InstructorsPage() {
   const searchParams = useSearchParams();
-  const page = Number(searchParams.get('page')) || 0;
+  const locale = useLocale();
+  const page = Number(searchParams.get("page")) || 0;
   const perPage = 6;
-  const pagedInstructors = instructors.slice(page * perPage, (page + 1) * perPage);
+  const [loading, setLoading] = useState(true);
+  const [instructors, setIsntructors] = useState([]);
+  const pagedInstructors = instructors.slice(
+    page * perPage,
+    (page + 1) * perPage
+  );
+
+  useEffect(() => {
+    const fetchIntructors = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(`${API_URL}/users/filter/?is_instructor=true`, {
+          headers: {
+            "Accept-Language": locale,
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
+        const data = await res.json();
+        setIsntructors(data);
+      } catch (error) {
+        console.error(error);
+        setIsntructors([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchIntructors();
+  }, [locale]);
 
   return (
     <div className="flex flex-col gap-8 justify-center items-center w-full">
       <HintNavigation
         links={[
-          { label: 'Главная', href: '../' },
-          { label: 'Инструкторы', href: '/instructors' },
+          { label: "Главная", href: "../" },
+          { label: "Инструкторы", href: "/instructors" },
         ]}
       />
       <Title color="green">Сертифицированные инструкторы KART</Title>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
-        {pagedInstructors?.map((instructor, idx) => (
-          <InstructorCard key={idx} instructor={instructor} />
+        {instructors?.map((instructor) => (
+          <InstructorCard
+            key={instructor.id}
+            firstName={instructor.first_name}
+            lastName={instructor.last_name}
+            position={instructor.position}
+            country={instructor.country}
+            contacts={instructor.contacts}
+          />
         ))}
       </div>
 
       <Pagination totalPages={Math.ceil(instructors.length / perPage)} />
 
-      <Link href='/criteria-instructor'><Button>Стать инструктором</Button></Link>
+      <Link href="/criteria-instructor">
+        <Button>Стать инструктором</Button>
+      </Link>
     </div>
   );
 }
